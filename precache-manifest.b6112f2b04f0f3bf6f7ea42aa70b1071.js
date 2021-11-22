42f5e65b874b37ec0c4f560d47646ce2098dfdfc
Chijioke Nna <nnachijioke@yahoo.com>
2021-11-22 15:22:51 +0100

    Updates

---
+++ precache-manifest.b6112f2b04f0f3bf6f7ea42aa70b1071.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dea735c43799a92280d1d1ce0c2cccd7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "07b757d260a2029c8acb",
+    "url": "./static/css/main.be601951.chunk.css"
+  },
+  {
+    "revision": "28cc919eb5acfb9012da",
+    "url": "./static/js/2.5b60e0ae.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.5b60e0ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07b757d260a2029c8acb",
+    "url": "./static/js/main.6c0de12a.chunk.js"
+  },
+  {
+    "revision": "6fce53c7c7713ebf61712cc2929746fa",
+    "url": "./static/js/main.6c0de12a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6bac22cee286ebccb31f",
+    "url": "./static/js/runtime-main.be4684bc.js"
+  }
+]);
